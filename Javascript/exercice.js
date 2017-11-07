7861d3a4a0af6b5af0274aa0bcc7304103f48251
sagara911@gmail.com <Lachaumiere1!>
2017-11-07 20:36:24 +0100

    force

---
+++ Javascript/exercice.js
@@ -0,0 +1,5 @@
+// var button =document.getElementById("special");
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+button.addEventListener("click",function(){
+    body.classList.toggle("bleue");});
